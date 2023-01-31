48f693d65e841819e1d82ec9a86cd148572c7b63
Katie <katiefaybehrmann@gmail.com>
2023-01-30 18:45:52 -0700

    Add a checkbox search form

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-function SearchForm(){
+function SearchForm() {
     return (
-        <div>
-            <h2>Search form for filtering projects</h2>
+        <div className="searchbar">
+            <form>
+                <label>
+                    Search by materials!
+                </label><br/><br/>
+                <input type="checkbox" id="yarn" name="yarn" value="Yarn" />
+                <label for="yarn">Yarn</label><br/>
+                <input type="checkbox" id="knittingNeedles" name="knittingNeedles" value="Knitting Needles" />
+                <label for="knittingNeedles">Knitting Needles</label><br/>
+                <input type="checkbox" id="embroidery" name="embroidery" value="Embroidery Floss" />
+                <label for="embroidery">Embroidery Floss</label><br/>
+                <input type="checkbox" id="crochetHook" name="crochetHook" value="Crochet Hook"/>
+                <label for="crochetHook">Crochet Hook</label><br/>
+                <input type="checkbox" id="needles" name="needles" value="Needles"/>
+                <label for="checkbox">Sewing Needles</label>
+            </form>
         </div>
     )
 }
