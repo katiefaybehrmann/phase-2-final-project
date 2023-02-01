d8772cd2945415e037ebdd7daf1b0ead49bd3736
Katie <katiefaybehrmann@gmail.com>
2023-01-31 18:46:39 -0700

    Add form to form page--no functionality

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,9 +1,28 @@
 import React from "react";
 
-function AddProject(){
+function AddProject({ handleAddProject }) {
     return (
         <div>
-            <h1 style={{ color: 'red' }}>This will be a controlled post form. Will not show up once navigation is added.</h1>
+            <h2>Add a Project</h2>
+            <form>
+                <input type="text" name="name" placeholder="Name" />
+                <br/>
+                <input type="text" name="image" placeholder="Image URL" />
+                <br/>
+                <input type="text" name="difficulty" placeholder="Difficulty" />
+                <br/>
+                <input type="radio" id="knitting" name="type" value="Knitting" />
+                <label for="knitting">Knitting</label><br />
+                <input type="radio" id="embroidery" name="type" value="Embroidery" />
+                <label for="embroidery">Embroidery</label><br />
+                <input type="radio" id="crochet" name="type" value="Crochet" />
+                <label for="crochet">Crochet</label><br />
+                <input type="radio" id="sewing" name="type" value="Sewing" />
+                <label for="sewing">Sewing</label><br/>
+                <input type="text" name="description" placeholder="Description"/>
+                <br/>
+                <button type="submit" >Add Project</button>
+            </form>
         </div>
     )
 }
