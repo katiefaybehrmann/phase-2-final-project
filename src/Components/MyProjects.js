b5c231833ed9419cbb13cfb9cc382b99ad336fd3
Katie <katiefaybehrmann@gmail.com>
2023-02-09 17:38:16 -0700

    Add additional styling to buttons

--- src/Components/MyProjects.js
+++ src/Components/MyProjects.js
@@ -3,6 +3,8 @@ import MyProjectsCard from "./MyProjectsCard";
 import uuid from "react-uuid";
 
 function MyProjects({myProjectList}){
+
+
     return (
         <div>
             <ul className="cards">{myProjectList.map(project => <MyProjectsCard key={uuid()} project={project} />)}</ul>
--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -7,7 +7,7 @@ function ProjectCard({ project, handleAddToMyList }) {
             <img src={project.image} alt={project.name}/>
             <h3>{project.name}</h3>
             <h4>Difficulty: {project.difficulty}</h4>
-            <button id={project.name} onClick={handleAddToMyList}>Add to My Projects</button>
+            <button className="btn" id={project.name} onClick={handleAddToMyList}>Add to My Projects</button>
         </li>
     )
 }
